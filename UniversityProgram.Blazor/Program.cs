008235c543d8319e64d72598583742fe70800d52
KarlosHakobyan <karloshakobyan99@gmail.com>
2025-04-16 03:55:29 +0400

    Add mudblazor

--- UniversityProgram.Blazor/Program.cs
+++ UniversityProgram.Blazor/Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using MudBlazor.Services;
 using UniversityProgram.Blazor.Apis;
 
 namespace UniversityProgram.Blazor
@@ -12,6 +13,8 @@ namespace UniversityProgram.Blazor
             builder.RootComponents.Add<App>("#app");
             builder.RootComponents.Add<HeadOutlet>("head::after");
 
+
+            builder.Services.AddMudServices();
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
             builder.Services.AddHttpClient<IStudentApi, StudentApi>(client =>
             {
