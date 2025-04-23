418e0f8a229d15e74ee6f3371c51127c0b084cdd
KarlosHakobyan <karloshakobyan99@gmail.com>
2025-04-23 04:09:34 +0400

    Add moq in blazor test.

--- UniversityProgramBlazor.Test/UnitTest1.cs
+++ UniversityProgramBlazor.Test/UnitTest1.cs
@@ -1,10 +1,15 @@
 ﻿using Bunit;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using Moq;
+using UniversityProgram.Blazor.Apis;
 using UniversityProgram.Blazor.Components;
+using UniversityProgram.Blazor.Models;
 
 
 namespace UniversityProgramBlazor.Test
 {
-    public class UnitTest1 : TestContext
+    public class UnitTest1 : BunitContext
     {
         [Fact]
         public void Test1()
@@ -19,6 +24,32 @@ namespace UniversityProgramBlazor.Test
 
             // Assert
             textComponent.MarkupMatches("<p id=\"MyText\">user name: Gago</p>");
+        }
+
+
+
+        [Fact]
+        public void NewTestPage_GetStudentName_Success()
+        {
+            // Arrange
+            Mock<IStudentApi> studentApiMock = new Mock<IStudentApi>();
+            List <StudentModel> models = new List<StudentModel>
+            {
+                new StudentModel { Id = 1, Name = "Aram" },
+                new StudentModel { Id = 2, Name = "Gago" }
+            };
+            studentApiMock.Setup(x => x.GetAll()).ReturnsAsync(models);
+            Services.TryAddScoped(e=>studentApiMock.Object);
+
+            var component = Render<NewTestPage>();
+            var button = component.Find(".get-students");
+            var textComponent = component.Find(".student-name");
+
+            // Act
+            button.Click();
+
+            // Assert
+            textComponent.MarkupMatches("<p class=\"student-name\">Aram</p>");
         }
     }
 }
